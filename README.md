eb359774b5c981d0864031a11f25be24cd0969a0
Lea haccoun <Lea haccoun>
2017-03-15 11:41:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HelloAndroid
+first lesson of the season
+
+This is a project that shows the basics of creating a one screen application with added features :
+keep the information when rotating the screen.
---
+++ app/src/main/java/huji/ac/il/helloandroid/MainActivity.java
@@ -0,0 +1,69 @@
+package huji.ac.il.helloandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    EditText mTextInput;
+    TextView mChatContent;
+    ImageView mSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextInput = (EditText) findViewById(R.id.text_input);
+        mSend = (ImageView) findViewById(R.id.send);
+        mTextInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    appendLine(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appendLine(mTextInput);
+            }
+        });
+
+
+        mChatContent = (TextView) findViewById(R.id.conversation);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save custom values into the bundle
+        // Always call the superclass so it can save the view hierarchy state
+
+        outState.putString("text", mTextInput.getText().toString());
+        outState.putString("tex", mChatContent.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+    @Override
+    protected void  onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        mTextInput.setText(savedInstanceState.getString("text"));
+        mChatContent.setText(savedInstanceState.getString("tex"));
+    }
+
+    private void appendLine(TextView v) {
+        StringBuilder currentChat = new StringBuilder(mChatContent.getText());
+        currentChat.append(v.getText()).append('\n');
+        mChatContent.setText(currentChat.toString());
+        mTextInput.setText("");
+    }
+}
